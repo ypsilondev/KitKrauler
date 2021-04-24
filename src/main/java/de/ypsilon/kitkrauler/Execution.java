fb769b4241074f69c7e8eda19f2db60e0e3ea103
Niklas @yDev <niklas@magic-computer.de>
2021-04-24 16:26:17 +0200

    Console support

---
+++ src/main/java/de/ypsilon/kitkrauler/Execution.java
@@ -0,0 +1,135 @@
+package de.ypsilon.kitkrauler;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class Execution {
+    private final WebDriver driver;
+    private WebDriverWait wait;
+    private String gguid;
+
+    private String uName;
+    private String pw;
+
+    private TrayIcon trayIcon;
+
+    private boolean firstInsert = true;
+    private HashMap<String, String> data = new HashMap<>();
+
+    public Execution(EdgeOptions driverOptions) throws AWTException {
+        this.driver = new EdgeDriver(driverOptions);
+
+        execute();
+    }
+
+    public Execution(EdgeOptions driverOptions, String[] credentials) throws AWTException {
+        this.uName = credentials[0];
+        this.pw = credentials[1];
+
+        this.driver = new EdgeDriver(driverOptions);
+        execute();
+    }
+
+    public void execute() throws AWTException {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(60).getSeconds());
+
+        if (SystemTray.isSupported()) {
+            SystemTray tray = SystemTray.getSystemTray();
+
+            Image img = Toolkit.getDefaultToolkit().getImage("images/img.jpg");
+            trayIcon = new TrayIcon(img, "KIT Krauler");
+            trayIcon.setImageAutoSize(true);
+            trayIcon.addActionListener(e -> {
+                trayIcon.displayMessage("KIT", "Ich gehe noch aber beim KIT arbeitet keiner", TrayIcon.MessageType.INFO);
+            });
+
+            tray.add(trayIcon);
+        }
+
+
+        try {
+            driver.get("https://campus.studium.kit.edu/");
+            System.out.println(driver.getTitle() + " - Das KIT bleibt handlungsfähig");
+
+            driver.findElement(By.id("hello")).findElement(By.className("login-link")).click();
+
+            // Login
+            login();
+
+            wait.until(presenceOfElementLocated(By.id("hello")));
+
+            driver.navigate().to("https://campus.studium.kit.edu/exams/registration.php");
+
+            wait.until(presenceOfElementLocated(By.id("registration")));
+            driver.switchTo().frame(driver.findElement(By.id("registration")));
+            gguid = driver.findElement(By.name("gguid")).getAttribute("value");
+
+            fetchData();
+
+            while (true) {
+                Thread.sleep(TimeUnit.SECONDS.toMillis(10));
+                update();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private void login() {
+        if (this.uName != null || this.pw != null) {
+            driver.findElement(By.id("name")).sendKeys(this.uName);
+            driver.findElement(By.id("password")).sendKeys(this.pw + Keys.ENTER);
+        }
+    }
+
+    private void update() {
+        driver.navigate().refresh();
+        wait.until(presenceOfElementLocated(By.name("gguid")));
+        gguid = driver.findElement(By.name("gguid")).getAttribute("value");
+        fetchData();
+    }
+
+    private void fetchData() {
+        WebElement content = driver.findElement(By.id("contract_" + gguid)).findElement(By.className("tablecontent"));
+        for (WebElement row : content.findElements(By.tagName("tr"))) {
+            List<WebElement> tds = row.findElements(By.tagName("td"));
+            String subj = tds.get(0).findElement(By.tagName("a")).getText();
+            String grade = row.findElement(By.className("nowrap")).getText();
+
+            addToHashMap(subj, grade);
+            firstInsert = false;
+        }
+    }
+
+    private void addToHashMap(String key, String value) {
+        if (!data.containsKey(key)) {
+            data.put(key, value);
+        } else {
+            if (!data.get(key).equals(value)) {
+                data.put(key, value);
+                if (!firstInsert) {
+                    String update = String.format("key updated (%s) to %s.%n", key, value);
+                    if (trayIcon != null) {
+                        trayIcon.displayMessage("KIT UPDATE!!11111", update, TrayIcon.MessageType.INFO);
+                    }
+                    System.out.println(update);
+                }
+            }
+        }
+    }
+}
--- src/main/java/de/ypsilon/kitkrauler/KitCrawler.java
+++ src/main/java/de/ypsilon/kitkrauler/KitCrawler.java
@@ -1,117 +1,42 @@
 package de.ypsilon.kitkrauler;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Time;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class KitCrawler {
+    private static final String U_PATTERN = "^(u[a-z]{4})$";
 
-    private static boolean firstInsert = true;
-    private static HashMap<String, String> data = new HashMap<>();
-    private static TrayIcon trayIcon;
-
-    private static WebDriver driver;
-    private static WebDriverWait wait;
-
-    private static boolean update = false;
-    private static String gguid;
-
-    public static void main(String[] args) throws AWTException {
-        driver = new EdgeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60).getSeconds());
-
-        if (SystemTray.isSupported()) {
-            SystemTray tray = SystemTray.getSystemTray();
-
-            Image img = Toolkit.getDefaultToolkit().getImage("images/img.jpg");
-            trayIcon = new TrayIcon(img, "KIT Krauler");
-            trayIcon.setImageAutoSize(true);
-            trayIcon.addActionListener(e -> {
-                trayIcon.displayMessage("KIT", "Ich gehe noch aber beim KIT arbeitet keiner", TrayIcon.MessageType.INFO);
-            });
-
-            tray.add(trayIcon);
-        }
-
-
+    public static void main(String[] args) {
+        System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
         try {
-            driver.get("https://campus.studium.kit.edu/");
-            System.out.println(driver.getTitle());
+            EdgeOptions edgeOptions = new EdgeOptions();
 
-            driver.findElement(By.id("hello")).findElement(By.className("login-link")).click();
+            if (args.length == 2) {
+                validateInput(args);
+                edgeOptions.addArguments("headless");
+                edgeOptions.addArguments("disable-gpu");
 
-            // Login
-
-            wait.until(presenceOfElementLocated(By.id("hello")));
-
-            driver.navigate().to("https://campus.studium.kit.edu/exams/registration.php");
-
-            wait.until(presenceOfElementLocated(By.id("registration")));
-            driver.switchTo().frame(driver.findElement(By.id("registration")));
-            gguid = driver.findElement(By.name("gguid")).getAttribute("value");
-
-            System.out.println(gguid);
-
-            fetchData();
-
-            while (true) {
-                Thread.sleep(TimeUnit.SECONDS.toMillis(10));
-                update();
+                new Execution(edgeOptions, args);
+            } else {
+                new Execution(edgeOptions);
             }
-        } catch (Exception e) {
+        } catch (AWTException e) {
             e.printStackTrace();
-        } finally {
-            driver.quit();
         }
     }
 
-    private static void update() throws InterruptedException {
-        update = true;
-        driver.navigate().refresh();
-        wait.until(presenceOfElementLocated(By.name("gguid")));
-        update = false;
-        gguid = driver.findElement(By.name("gguid")).getAttribute("value");
-        fetchData();
-    }
+    private static void validateInput(String[] args) {
+        Matcher uMatcher = Pattern.compile(U_PATTERN).matcher(args[0]);
+        Scanner scanner = new Scanner(System.in);
 
-    private static void fetchData() {
-        WebElement content = driver.findElement(By.id("contract_" + gguid)).findElement(By.className("tablecontent"));
-        for (WebElement row : content.findElements(By.tagName("tr"))) {
-            List<WebElement> tds = row.findElements(By.tagName("td"));
-            String subj = tds.get(0).findElement(By.tagName("a")).getText();
-            String grade = row.findElement(By.className("nowrap")).getText();
-
-            addToHashMap(subj, grade);
-            firstInsert = false;
+        while (!uMatcher.matches()) {
+            System.out.println("Inkorrektes U-Kürzel");
+            args[0] = scanner.nextLine();
+            uMatcher = Pattern.compile(U_PATTERN).matcher(args[0]);
         }
     }
-
-    private static void addToHashMap(String key, String value) {
-        if (!data.containsKey(key)) {
-            data.put(key, value);
-        } else {
-            if (!data.get(key).equals(value)) {
-                data.put(key, value);
-                if (!firstInsert) {
-                    String update = String.format("key updated (%s) to %s.%n", key, value);
-                    if (trayIcon != null) {
-                        trayIcon.displayMessage("KIT UPDATE!!11111", update, TrayIcon.MessageType.INFO);
-                    }
-                    System.out.println(update);
-                }
-            }
-        }
-    }
-
 }
